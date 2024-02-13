bed95d1552aec0284b7f979a76e0f74bd766e01c
KubaGlegola <kubaglegola@gmail.com>
2024-02-13 20:11:11 +0100

    fixed aria-label in pagination component

--- src/ui/molecules/Pagination.tsx
+++ src/ui/molecules/Pagination.tsx
@@ -11,7 +11,7 @@ export const Pagination = ({ totalPages }: PaginationProps) => {
 			{emptyArray.map((_, index) => {
 				const pageNumber = index + 1;
 				return (
-					<li key={pageNumber} aria-label="pagination">
+					<li key={pageNumber} aria-label={`pagination/${pageNumber}`}>
 						<ActiveLink href={`/products/${pageNumber}`} exact={true}>
 							{pageNumber}
 						</ActiveLink>
