813185b54e3769293f2841debe2e1c6932d9b114
KubaGlegola <kubaglegola@gmail.com>
2024-02-20 20:28:42 +0100

    removed unused type file

--- src/ui/atoms/ProductListItemDescription.tsx
+++ src/ui/atoms/ProductListItemDescription.tsx
@@ -1,20 +1,21 @@
-import { type ProductItemType } from "@/ui/types";
+import { type ProductListItemFragment } from "@/gql/graphql";
 import { formatMoney } from "@/utils";
 
 type ProductListItemDescriptionProps = {
-	product: ProductItemType;
+	product: ProductListItemFragment;
 };
 
-export const ProductListItemDescription = ({
-	product: { name, category, price },
-}: ProductListItemDescriptionProps) => {
+export const ProductListItemDescription = (props: ProductListItemDescriptionProps) => {
+	const { name, categories, price } = props.product;
 	return (
 		<div className="mt-2 flex justify-between">
 			<div>
 				<h3 className="text-sm font-semibold text-gray-700">{name}</h3>
-				<p className="text-sm text-gray-500">
-					<span className="sr-only">Kategoria: </span> {category}
-				</p>
+				{categories[0] && (
+					<p className="text-sm text-gray-500">
+						<span className="sr-only">Kategoria: </span> {categories[0].name}
+					</p>
+				)}
 			</div>
 			<p className="text-sm font-medium text-gray-900">
 				<span className="sr-only">Cena: </span> {formatMoney(price)}
