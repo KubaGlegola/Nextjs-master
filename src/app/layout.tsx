331c7d03597eeb32f6a770a951245ff34b3513d8
KubaGlegola <jakub.glegola@brandsmanago.pl>
2024-03-12 20:09:56 +0100

    add clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Navbar } from "@/ui/organism/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,13 +17,15 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>
-				<header className="sticky top-0 z-20 border-b bg-white bg-opacity-60 backdrop-blur-lg">
-					<Navbar />
-				</header>
-				<div className="mx-auto max-w-lg p-4 md:max-w-4xl lg:max-w-7xl ">{children}</div>
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body className={inter.className}>
+					<header className="sticky top-0 z-20 border-b bg-white bg-opacity-60 backdrop-blur-lg">
+						<Navbar />
+					</header>
+					<div className="mx-auto max-w-lg p-4 md:max-w-4xl lg:max-w-7xl ">{children}</div>
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
